55dde4173f7d0b135316fc08c4a7d685dfeef519
JayXie9499 <tsum950210@gmail.com>
2025-12-24 10:22:19 +0800

    feat(user): Implement get current user posts endpoint

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
 from ..database import get_db
-from ..models import User
+from ..models import User, Post
 from ..dependencies import auth_dependency
 
 router = APIRouter()
@@ -26,6 +26,22 @@ async def read_current_user(
 		print(e)
 		response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
 		return {"message": "An error occurred while fetching user data"}
+
+
+@router.get("/me/posts")
+async def read_current_user_posts(
+	response: Response,
+	jwt_payload: dict[str, Any] = Depends(auth_dependency),
+	db: Session = Depends(get_db),
+):
+	try:
+		posts = db.query(Post).filter(Post.owner_id == jwt_payload["user_id"]).all()
+
+		return {"message": "Successfully fetched user's posts", "data": posts}
+	except SQLAlchemyError as e:
+		print(e)
+		response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+		return {"message": "An error occurred while fetching user's posts"}
 
 
 @router.get("/{id}")
