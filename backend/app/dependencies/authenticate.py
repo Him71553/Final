8179955d573feb483abd67e7375e837e2ed60597
JayXie9499 <tsum950210@gmail.com>
2025-12-24 10:36:33 +0800

    refactor(auth): Switch token extraction source to cookies
    - Update `authenticate.py` to retrieve the JWT token from the `jwt_token` cookie
    - Remove the logic that extracts the token from the `Authorization` header

--- backend/app/dependencies/authenticate.py
+++ backend/app/dependencies/authenticate.py
@@ -1,12 +1,12 @@
-from fastapi import HTTPException
+from typing import Annotated
+from fastapi import Cookie, HTTPException
 from ..security import verify_jwt_token
 
 
-def auth_dependency(authorization: str | None = None):
-	if authorization is None:
+def auth_dependency(jwt_token: Annotated[str | None, Cookie()] = None):
+	if jwt_token is None:
 		raise HTTPException(status_code=401, detail="Unauthorized")
 
-	jwt_token = authorization.split(" ")[1]
 	jwt_payload = verify_jwt_token(jwt_token)
 	if jwt_payload is None:
 		raise HTTPException(status_code=401, detail="Unauthorized")
