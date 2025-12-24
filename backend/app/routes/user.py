b84d8042f7b6d3b457b15956d716a9ab323db330
JayXie9499 <tsum950210@gmail.com>
2025-12-24 10:10:35 +0800

    feat(user): Implement get current user endpoint

--- backend/app/routes/user.py
+++ backend/app/routes/user.py
@@ -1,3 +1,28 @@
-from fastapi import APIRouter
+from typing import Any
+from fastapi import APIRouter, Depends, Response, status
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+from ..database import get_db
+from ..models import User
+from ..dependencies import auth_dependency
 
 router = APIRouter()
+
+
+@router.get("/me")
+async def read_current_user(
+	response: Response,
+	jwt_payload: dict[str, Any] = Depends(auth_dependency),
+	db: Session = Depends(get_db),
+):
+	try:
+		user = db.query(User).filter(User.id == jwt_payload["user_id"]).first()
+		if not user:
+			response.status_code = status.HTTP_404_NOT_FOUND
+			return {"message": "User not found"}
+
+		return {"message": "Successfully fetched user data", "data": user}
+	except SQLAlchemyError as e:
+		print(e)
+		response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+		return {"message": "An error occurred while fetching user data"}
